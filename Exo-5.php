20849463d3fff44354db55176dbad4e6e81cac25
FADLI Saïd <saidfadli224@gmail.com>
2025-02-14 12:05:49 +0100

    terminer

--- Exo-5.php
+++ Exo-5.php
@@ -1,7 +1,7 @@
 <?php
 
-$month = "Février";
-$specialday = 0;
+$month = mb_strtoupper(string: "Mars",encoding: "utf8");
+$specialday = 17;
 
 ?>
 
@@ -16,63 +16,59 @@ $specialday = 0;
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body class="container">
+<body class="container text-center">
 
-    <p class="mt-3 text-uppercase text-danger fw-bold"><?php echo "$month" ?></p>
-    <p style="word-spacing: 2rem;">L M M J V S D</p>
-    <div class="d-flex">
+    <p class="mt-3 text-danger fw-bold ms-4"><?php echo "$month" ?></p>
+    <div class="d-flex justify-content-center">
 
-        <?php for ($i = 1; $i <= 7; $i++) {
-            echo `<p class="align-middle">`;
-            for ($totalday = $i; $totalday <= 31; $totalday += 7) {
-                "$totalday" . "<br>";
+        <?php
+
+        for ($i = 1; $i <= 7; $i++) {
+            $gris = ($i == 6 || $i == 7) ? "text-secondary" : "";
+            switch ($i) {
+                case 1:
+                    $day = "L";
+                    break;
+
+                case 2:
+                    $day = "M";
+                    break;
+
+                case 3:
+                    $day = "M";
+                    break;
+
+                case 4:
+                    $day = "J";
+                    break;
+
+                case 5:
+                    $day = "V";
+                    break;
+
+                case 6:
+                    $day = "S";
+                    break;
+
+                case 7:
+                    $day = "D";
+                    break;
+
+                default:
+                    break;
             }
-            `</p>`;
+
+
+            echo "<p class='ms-4 $gris fw-bold'>";
+            echo "$day" . "<br>";
+            for ($totalday = $i; $totalday <= 31; $totalday += 7) {
+                $whiteOnRed = $totalday == $specialday ? "text-light bg-danger border rounded-circle px-1 py-1" : "";
+                echo "<span class='$whiteOnRed'>$totalday</span>" . "<br>";
+            }
+            "</p>";
         }
 
         ?>
-        <!-- 
-        <p class="align-middle">
-            <?php for ($totalday = 1; $totalday <= 31; $totalday += 7) {
-                echo "$totalday" . "<br>";
-            } ?>
-        </p>
-
-        <p class="ms-4">
-            <?php for ($totalday = 2; $totalday <= 31; $totalday += 7) {
-                echo "$totalday" . "<br>";
-            } ?>
-        </p>
-
-        <p class="ms-4">
-            <?php for ($totalday = 3; $totalday <= 31; $totalday += 7) {
-                echo "$totalday" . "<br>";
-            } ?>
-        </p>
-
-        <p class="ms-4">
-            <?php for ($totalday = 4; $totalday <= 31; $totalday += 7) {
-                echo "$totalday" . "<br>";
-            } ?>
-        </p>
-
-        <p class="ms-4">
-            <?php for ($totalday = 5; $totalday <= 31; $totalday += 7) {
-                echo "$totalday" . "<br>";
-            } ?>
-        </p>
-
-        <p class="ms-4">
-            <?php for ($totalday = 6; $totalday <= 31; $totalday += 7) {
-                echo "$totalday" . "<br>";
-            } ?>
-        </p>
-
-        <p class="ms-4">
-            <?php for ($totalday = 7; $totalday <= 31; $totalday += 7) {
-                echo "$totalday" . "<br>";
-            } ?>
-        </p> -->
 
     </div>
 
